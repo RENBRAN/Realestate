2ce1ede1684b3f86665aa48a54030d0650fdd367
RENBRAN <139387428+RENBRAN@users.noreply.github.com>
2024-04-07 23:51:20 +0400

    Delete real_estate_manager-main directory

--- real_estate_manager-main/models/property_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class PropertyType(models.Model):
-    _name = 'property.type'
-    _description = 'Property Type'
-
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description', required=True)
