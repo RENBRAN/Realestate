70aad5fa928ca9d1d3eda8877063e2a5b2746363
RenbranMadelo <renbranmadelo@gmail.com>
2024-04-08 01:21:52 +0400

    addons

---
+++ eg_invoice_receipt_report/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Invoice Receipt Report",
+
+    'version': "17.0",
+
+    "summary": "Invoice Receipt Report",
+
+    'author': "INKERP",
+    
+    'website': "https://www.inkerp.com",
+
+    "depends": ['account'],
+
+    "data": ["views/account_move_view.xml",
+             ],
+
+    'images': ['static/description/banner.png'],
+    'license': "OPL-1",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
