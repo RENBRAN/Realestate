0e725354072acd85a5812a9515c0ba3a67cb05e9
RenbranMadelo <renbranmadelo@gmail.com>
2024-04-07 23:21:43 +0400

    s

--- Estate-Management-master/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import owner, tenant, visitor
-from . import property, property_maintanance, property_asset, property_survey
-from . import contract_management, lease_agreement, property_offer
-from . import transaction
