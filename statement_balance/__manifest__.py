ae8b26f72bf941fbb029f5e4da427620d4b446ea
RenbranMadelo <renbranmadelo@gmail.com>
2024-04-18 00:44:50 +0400

    yes

---
+++ statement_balance/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Statement Balance",
+
+    'summary': """
+        Statement Balance""",
+
+    'description': """
+        Add new field to get sum of statement lines.
+    """,
+
+    'author': "Bola Ayman",
+
+    'category': 'Accounting/Accounting',
+    
+    'version': '17.0.0.1',
+
+    'license': 'AGPL-3',
+
+
+    'depends': ['base', 'account'],
+
+    'data': [
+        'views/account_bank_statement_view.xml',
+    ],
+
+    'live_test_url': 'https://www.youtube.com/watch?v=SpFJmHVyMKg',
+
+}
